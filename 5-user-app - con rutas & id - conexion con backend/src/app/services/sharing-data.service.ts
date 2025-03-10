c9451142b134108e04fcb77d3925736016b8ff93
TonyMoral <134309497+TonyMoral@users.noreply.github.com>
2025-03-10 20:33:57 +0100

    Add files via upload

---
+++ 5-user-app - con rutas & id - conexion con backend/src/app/services/sharing-data.service.ts
@@ -0,0 +1,47 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharingDataService {
+
+  private _newUserEventEmitter: EventEmitter<User> = new EventEmitter();
+
+  private _idUserEventEmitter = new EventEmitter();
+
+  private _findUserByIdEventEmitter = new EventEmitter();
+
+  private _selectUserEventEmitter = new EventEmitter();
+  
+  private _errorsUserFormEventEmitter = new EventEmitter();
+
+  private _pageUsersEventEmitter = new EventEmitter();
+  
+  constructor() { }
+
+  get pageUsersEventEmitter(){
+    return this._pageUsersEventEmitter;
+  }
+
+  get errorsUserFormEventEmitter(){
+    return this._errorsUserFormEventEmitter;
+  }
+
+  get newUserEventEmitter(): EventEmitter<User>{
+    return this._newUserEventEmitter;
+  }
+
+  get idUserEventEmitter(): EventEmitter<number>{
+    return this._idUserEventEmitter;
+  }
+
+  get findUserByIdEventEmitter() {
+    return this._findUserByIdEventEmitter;
+  }
+
+  get selectUserEventEmitter(){
+    return this._selectUserEventEmitter;
+  }
+
+}
