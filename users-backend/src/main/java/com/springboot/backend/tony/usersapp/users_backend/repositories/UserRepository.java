f6805014fc522850480cf22664ea2295c2f0b9c5
TonyMoral <134309497+TonyMoral@users.noreply.github.com>
2025-03-10 20:30:11 +0100

    Add files via upload

---
+++ users-backend/src/main/java/com/springboot/backend/tony/usersapp/users_backend/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.springboot.backend.tony.usersapp.users_backend.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+import com.springboot.backend.tony.usersapp.users_backend.entities.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Page<User> findAll(Pageable pageable);
+}
